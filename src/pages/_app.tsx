6c42b0b41700d09213fd0abcd050b89ba4f6fa8a
= <=>
2023-10-18 21:25:52 -0300

    fix app bugs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { globalStyles } from 'styles/global'
 
 globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: any) {
   return (
     <>
       <Analytics />
